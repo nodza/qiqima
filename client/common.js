3553fe8878e4f28c22b7c4a6ace10205da0747c9
nodza <noel.hwande@gmail.com>
2016-09-22 23:11:38 -0500

    Displaying posts dynamically

--- client/common.js
+++ client/common.js
@@ -1,6 +1,15 @@
 Meteor.startup(function () {
     AccountsEntry.config({
         homeRoute: '/',
-        dashboardRoute: '/'
+        dashboardRoute: '/',
+        passwordSignupFields: 'USERNAME_AND_EMAIL'
     });
+    Accounts.ui.config({
+        passwordSignupFields: 'USERNAME_AND_EMAIL'
+    });
+});
+
+Template.registerHelper('getProfileImg', function (userId) {
+   var imgUrl = UserImages.findOne({userId: userId }).image;
+    return imgUrl;
 });
